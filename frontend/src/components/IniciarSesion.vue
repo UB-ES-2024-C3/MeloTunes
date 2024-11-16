1da0d443b1c1ca944b965a3562d5d66b557fdbeb
EnricArtero <eartergo41@alumnes.ub.edu>
2024-11-17 00:40:17 +0100

    visual patches

--- frontend/src/components/IniciarSesion.vue
+++ frontend/src/components/IniciarSesion.vue
@@ -29,7 +29,7 @@
 
         <div v-if="error" class="error">{{ error }}</div>
 
-        <button type="submit" class="red-button">Log in</button>
+        <button type="submit" class="submit-btn">Log in</button>
       </form>
 
       <p><a href="#">Forgot your password?</a></p>
@@ -63,7 +63,6 @@ export default {
           this.$router.push({ path: '/home', query: { email: this.email, logged: this.is_authenticated, token: this.token } })
         })
         .catch((error) => {
-          // eslint-disable-next-line
           console.error(error)
           alert('Username or Password incorrect')
         })
@@ -72,16 +71,11 @@ export default {
       this.showPassword = !this.showPassword
     },
     handleSubmit () {
-      // Verifica si los campos están vacíos antes de hacer login
       if (!this.email.trim() || !this.password.trim()) {
-        this.error = 'Por favor, complete todos los campos' // Mensaje de error
-        return // Detiene el flujo y no llama a `login`
+        this.error = 'Por favor, complete todos los campos'
+        return
       }
-
-      // Limpia el mensaje de error si pasa la validación
       this.error = null
-
-      // Llama al método login
       this.login()
     }
   }
@@ -107,7 +101,7 @@ body, html {
   top: 0;
   left: 0;
   width: 100%;
-  height: 150%;
+  height: 100%;
   background: radial-gradient(circle, rgba(255, 0, 0, 0.5), rgba(0, 0, 0, 0.8));
   overflow: hidden;
   z-index: 1;
@@ -133,27 +127,47 @@ body, html {
 }
 
 .container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   text-align: center;
   justify-content: center;
   background-color: #717d7e;
   padding: 20px;
-  margin-top: 5%;
-  border-radius: 10px;
+  border-radius: 15px;
   width: 700px;
-  position: relative;
-  z-index: 10;
+  height: auto;
+  max-height: 80vh;
+  overflow-y: auto;
+  z-index: 2;
+  box-sizing: border-box;
+}
+
+/* Ocultar barra de desplazamiento */
+.container::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+
+.container {
+  scrollbar-width: none;
+  -ms-overflow-style: none;
 }
 
 .close-btn {
   position: absolute;
   top: 10px;
   right: 10px;
-  background-color: transparent;
-  color: white;
-  font-size: 20px;
-  font-weight: bold;
+  background: none;
   border: none;
+  color: white;
+  font-size: 24px;
   cursor: pointer;
+}
+
+.close-btn:hover {
+  color: #ff4d4d;
 }
 
 .container h1 {
@@ -162,7 +176,7 @@ body, html {
 
 .container input[type="email"],
 .container input[type="password"] {
-  width: 37%;
+  width: 60%;
   padding: 10px;
   margin: 10px 0;
   border: none;
@@ -174,17 +188,8 @@ body, html {
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 40%;
+  width: 100%;
   margin: 0 auto;
-}
-
-.password-container input {
-  flex: 1;
-  border: none;
-  padding: 10px;
-  border-radius: 5px;
-  font-size: 16px;
-  margin: 10px auto;
 }
 
 .password-container img {
@@ -194,15 +199,20 @@ body, html {
   cursor: pointer;
 }
 
-.container input[type="submit"] {
-  width: 40%;
-  padding: 10px;
+.submit-btn {
+  width: 60%;
+  padding: 15px;
   margin-top: 10px;
   border: none;
   border-radius: 5px;
   background-color: red;
   color: white;
-  font-size: 16px;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+.submit-btn:hover {
+  background-color: red;
 }
 
 .container a {
@@ -214,20 +224,8 @@ body, html {
   text-decoration: underline;
 }
 
-.red-button {
-  background-color: red;
-  color: white;
-  border: none;
-  padding: 10px;
-  border-radius: 5px;
-}
-
 .error {
   color: red;
   font-size: 14px;
-}
-
-.red-text {
-  color: red;
 }
 </style>
--- frontend/src/components/Registrarse.vue
+++ frontend/src/components/Registrarse.vue
@@ -1,270 +1,232 @@
 <template>
   <div>
-    <div class ="background-animation"></div>
+    <div class="background-animation"></div>
 
     <div class="container">
       <button class="close-btn" @click="cerrarPopup">X</button>
-      <img src="@/assets/Im_logo.png" alt="Logo"><br>
+      <img src="../assets/Im_logo.png" alt="Logo"><br>
       <br>
       <h1>Completa tu Registro</h1>
 
-        <form>
-            <!-- Campo del correo electrónico -->
-            <label for="email">Email</label><br>
-            <input type="text" id="email" name="email" placeholder="name@domain.com"><br>
+      <form>
+        <!-- Campo del correo electrónico -->
+        <label for="email">Email</label><br>
+        <input type="text" id="email" name="email" placeholder="name@domain.com" class="form-input"><br>
 
-            <!-- Campo de contraseña -->
-            <label for="password">Contraseña</label><br>
-            <div class="password-container">
-                <input type="password" id="password" name="password" placeholder="Introduce tu contraseña">
-                <img id="togglePassword" src="Im_ojo2.png" alt="Mostrar/Ocultar Contraseña" @click="togglePassword('password')">
-            </div>
+        <!-- Campo de contraseña -->
+        <label for="password">Contraseña</label><br>
+        <div class="password-container">
+          <input :type="showPassword ? 'text' : 'password'" id="password" name="password" placeholder="Introduce tu contraseña" class="form-input">
+          <img :src="showPassword ? require('../assets/ojo.png') : require('../assets/ojo2.png')" alt="Mostrar/Ocultar Contraseña" @click="togglePassword">
+        </div>
 
-            <!-- Campo de confirmar contraseña -->
-            <label for="confirm_password">Confirmar Contraseña</label><br>
-            <div class="password-container">
-                <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirma tu contraseña">
-                <img id="toggleConfirmPassword" src="Im_ojo2.png" alt="Mostrar/Ocultar Contraseña" @click="togglePassword('confirm_password')">
-            </div>
+        <!-- Campo de confirmar contraseña -->
+        <label for="confirm_password">Confirmar Contraseña</label><br>
+        <div class="password-container">
+          <input :type="showConfirmPassword ? 'text' : 'password'" id="confirm_password" name="confirm_password" placeholder="Confirma tu contraseña" class="form-input">
+          <img :src="showConfirmPassword ? require('../assets/ojo.png') : require('../assets/ojo2.png')" alt="Mostrar/Ocultar Contraseña" @click="toggleConfirmPassword">
+        </div>
 
-            <!-- Mostrar error si las contraseñas no coinciden o no se cumplen las condiciones -->
-            <div id="error-password" class="error"></div>
+        <!-- Mostrar error si las contraseñas no coinciden o no se cumplen las condiciones -->
+        <div id="error-password" class="error"></div>
 
-            <!-- Campo de nombre -->
-            <label for="nombre">Nombre</label><br>
-            <input type="text" id="nombre" name="nombre" placeholder="Introduce tu nombre"><br>
+        <!-- Campo de nombre -->
+        <label for="nombre">Nombre</label><br>
+        <input type="text" id="nombre" name="nombre" placeholder="Introduce tu nombre" class="form-input"><br>
 
-          <!-- Campo de apellido -->
-          <label for="nombre">Apellido</label><br>
-          <input type="text" id="apellido" name="apellido" placeholder="Introduce tu apellido"><br>
+        <!-- Campo de apellido -->
+        <label for="apellido">Apellido</label><br>
+        <input type="text" id="apellido" name="apellido" placeholder="Introduce tu apellido" class="form-input"><br>
 
-            <!-- Campo de fecha de nacimiento -->
-            <label for="fecha_nacimiento">Fecha de Nacimiento</label><br>
-            <input type="date" id="fecha_nacimiento" name="fecha_nacimiento"><br>
+        <!-- Campo de fecha de nacimiento -->
+        <label for="fecha_nacimiento">Fecha de Nacimiento</label><br>
+        <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" class="form-input"><br>
 
-            <!-- Mostrar error si la persona es menor de 16 años -->
-            <div id="error-edad" class="error"></div>
+        <!-- Mostrar error si la persona es menor de 16 años -->
+        <div id="error-edad" class="error"></div>
 
-            <!-- Botón de enviar -->
-            <input type="button" value="Completar Registro" @click="registerUser()" class="submit-btn">
+        <!-- Botón de enviar -->
+        <input type="button" value="Completar Registro" @click="registerUser()" class="submit-btn">
 
-          <!-- Texto para iniciar sesión -->
-          <p class="login-text">
-            ¿Ya tienes una cuenta? <a href="/login">Inicia sesión aquí</a>
-          </p>
-
-        </form>
-
+        <!-- Texto para iniciar sesión -->
+        <p class="login-text">
+          ¿Ya tienes una cuenta? <a href="/login">Inicia sesión aquí</a>
+        </p>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-
 import RegisterService from '../services/RegisterService'
 
 export default {
   name: 'Registrarse',
   data () {
+    return {
+      showPassword: false, // Controla la visibilidad de la contraseña
+      showConfirmPassword: false // Controla la visibilidad de la confirmación de la contraseña
+    }
   },
   methods: {
-    validateEmail () {
-      // eslint-disable-next-line
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
-        this.msg['email'] = 'Please enter a valid email address'
-      } else {
-        this.msg['email'] = ''
-      }
-    },
-    registerUser () {
-      RegisterService.registerUser(document.getElementById('email').value, document.getElementById('nombre').value, document.getElementById('apellido').value, document.getElementById('password').value)
-      this.$router.push('/login')
-    },
     cerrarPopup () {
       this.$router.replace({ path: '/home' })
     },
-    togglePassword (fieldId) {
-      const field = document.getElementById(fieldId)
-      field.type = field.type === 'password' ? 'text' : 'password'
+    togglePassword () {
+      this.showPassword = !this.showPassword // Alterna el estado del primer campo de contraseña
+    },
+    toggleConfirmPassword () {
+      this.showConfirmPassword = !this.showConfirmPassword // Alterna el estado del segundo campo de contraseña
+    },
+    registerUser () {
+      const email = document.getElementById('email').value
+      const nombre = document.getElementById('nombre').value
+      const apellido = document.getElementById('apellido').value
+      const password = document.getElementById('password').value
+
+      RegisterService.registerUser(email, nombre, apellido, password)
+        .then(() => {
+          this.$router.push('/login')
+        })
+        .catch((error) => {
+          console.error(error)
+          alert('Error al registrar el usuario.')
+        })
     }
   }
 }
 </script>
 
-<!-- Estilos para el componente -->
 <style scoped>
-        body, html {
-            background-color: black;
-            font-family: Arial, sans-serif;
-            color: white;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            overflow: hidden; /* Evitar el desplazamiento en toda la página */
-            position: relative;
-        }
+body, html {
+  background-color: black;
+  font-family: Arial, sans-serif;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  overflow: hidden;
+}
 
-        /* Fondo animado */
-        .background-animation {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background: radial-gradient(circle, rgba(255, 0, 0, 0.5), rgba(0, 0, 0, 0.8));
-          overflow: hidden;
-          z-index: 1;
-        }
+/* Fondo animado */
+.background-animation {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: radial-gradient(circle, rgba(255, 0, 0, 0.5), rgba(0, 0, 0, 0.8));
+  overflow: hidden;
+  z-index: 1;
+}
 
-        .background-animation::before {
-          content: '';
-          position: absolute;
-          width: 200%;
-          height: 200%;
-          background-image: linear-gradient(60deg, rgba(255, 0, 0, 0.3) 25%, transparent 25%, transparent 75%, rgba(255, 0, 0, 0.3) 75%);
-          background-size: 30px 30px;
-          animation: move 4s linear infinite;
-        }
+.background-animation::before {
+  content: '';
+  position: absolute;
+  width: 200%;
+  height: 200%;
+  background-image: linear-gradient(60deg, rgba(255, 0, 0, 0.3) 25%, transparent 25%, transparent 75%, rgba(255, 0, 0, 0.3) 75%);
+  background-size: 30px 30px;
+  animation: move 4s linear infinite;
+}
 
-        @keyframes move {
-          0% {
-            background-position: 0 0;
-          }
-          100% {
-            background-position: 30px 30px;
-          }
-        }
+@keyframes move {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 30px 30px;
+  }
+}
 
-        .container {
-            position: absolute;
-            top: 50%; /* Centramos en el eje vertical */
-            left: 50%; /* Centramos en el eje horizontal */
-            transform: translate(-50%, -50%); /* Ajustamos el centro exacto */
-            text-align: center;
-            justify-content: center;
-            background-color: #717d7e;
-            padding: 20px;
-            border-radius: 10px;
-            width: 700px;
-            max-height: 80vh; /* Limitar la altura para evitar que sea demasiado alta */
-            overflow-y: auto; /* Hacer que el contenido sea desplazable solo dentro del recuadro */
-            z-index: 2;
-        }
-        .container h1 {
-            font-size: 24px;
-        }
-        .container input[type="text"],
-        .container input[type="date"],
-        .container input[type="tel"],
-        .container input[type="password"] {
-            width: 37%;
-            padding: 10px;
-            margin: 10px 0;
-            border: none;
-            border-radius: 5px;
-        }
-        .container input[type="email"] {
-            width: 37%;
-            padding: 10px;
-            margin: 10px 0;
-            border: none;
-            border-radius: 5px;
-            background-color: #ccc;
-            color: #666;
-        }
-        .password-container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 37%;
-            margin: 0px auto;
-        }
-        .password-container input {
-            flex: 1;
-            border: none;
-            padding: 10px;
-            border-radius: 5px;
-            font-size: 16px;
-            margin: 10px auto;
-        }
-        .password-container img {
-            width: 45px;
-            height: 40px;
-            margin-left: -45px;
-            cursor: pointer;
-        }
-        .submit-btn {
-          width: 37%;
-          padding: 15px;
-          margin-top: 10px;
-          border: none;
-          border-radius: 5px;
-          background-color: red;
-          color: white;
-          font-size: 18px;
-          cursor: pointer;
-        }
-        .submit-btn:hover {
-          background-color: darkred;
-        }
-        .login-text {
-          margin-top: 20px;
-          font-size: 15px;
-          color: white;
-        }
-        .login-text a {
-          color: #00f;
-          text-decoration: underline;
-        }
-        .login-text a:hover {
-          color: yellow;
-        }
-        .logo {
-          width: 100px;
-          margin-bottom: 20px;
-        }
-        .container input[type="submit"] {
-            width: 40%;
-            padding: 10px;
-            margin-top: 10px;
-            border: none;
-            border-radius: 5px;
-            background-color: red;
-            color: white;
-            font-size: 16px;
-        }
-        .container a {
-            color: white;
-            text-decoration: none;
-        }
-        .container a:hover {
-            text-decoration: underline;
-        }
-        .error {
-            color: rgb(187, 255, 41);
-            font-size: 14px;
-        }
-        .error-box {
-            background-color: #f8d7da;
-            color: #721c24;
-            border: 1px solid #f5c6cb;
-            padding: 10px;
-            border-radius: 5px;
-            margin: 10px 0;
-        }
-        .close-btn{
-          position: absolute;
-          top: 10px;
-          right: 10px;
-          background: none;
-          border: none;
-          color: white;
-          font-size: 24px;
-          cursor: pointer;
-        }
-        .close-btn:hover {
-          color: #ff4d4d;
-        }
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  justify-content: center;
+  background-color: #717d7e;
+  padding: 20px;
+  border-radius: 15px;
+  width: 700px;
+  height: auto; /* Altura automática según contenido */
+  max-height: 80vh; /* Limitar la altura máxima */
+  overflow-y: auto; /* Permitir desplazamiento interno */
+  z-index: 2;
+  box-sizing: border-box; /* Asegura que el padding no afecte las dimensiones */
+}
+
+/* Ocultar barra de desplazamiento en navegadores modernos */
+.container::-webkit-scrollbar {
+  width: 0; /* Anchura de la barra de desplazamiento */
+  height: 0; /* Altura de la barra de desplazamiento horizontal */
+}
+
+.container {
+  scrollbar-width: none; /* Firefox: Oculta la barra de desplazamiento */
+  -ms-overflow-style: none; /* IE 10+ */
+}
+
+.password-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: 0px auto;
+}
+
+.password-container img {
+  width: 45px;
+  height: 40px;
+  margin-left: -45px;
+  cursor: pointer;
+}
+
+.form-input {
+  width: 60%;
+  padding: 10px;
+  margin: 10px 0;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+.submit-btn {
+  width: 60%;
+  padding: 15px;
+  margin-top: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: red;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+.submit-btn:hover {
+  background-color: darkred;
+}
+
+.error {
+  color: rgb(187, 255, 41);
+  font-size: 14px;
+}
+
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 24px;
+  cursor: pointer;
+}
+
+.close-btn:hover {
+  color: #ff4d4d;
+}
 </style>
