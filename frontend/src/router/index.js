d5599272483f2e2bc04245464b15b104bc4d1da3
0-Madder <92030395+0-Madder@users.noreply.github.com>
2024-11-19 00:31:09 +0100

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,7 +10,7 @@ import PerfilUser from '@/components/Perfil_user'
 
 Vue.use(Router)
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
