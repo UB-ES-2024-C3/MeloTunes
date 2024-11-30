54782ee61b18cef8440a942e92d9c4222e466373
madder <pablomartinezuni@gmail.com>
2024-11-30 21:38:49 +0100

    Lil preprod test

--- frontend/src/services/SongService.js
+++ frontend/src/services/SongService.js
@@ -2,7 +2,7 @@ import http from '../http-common'
 
 class SongService {
   getAll () {
-    return http.get('/api/v1/songs')
+    return http.get('/api/v1/songs/')
       .then((res) => {
         return res
       })
