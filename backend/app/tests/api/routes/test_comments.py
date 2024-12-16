bbdbda18b1c96d0bce0de30de27aa35aa249c81b
Sara <sromergo11@alumnes.ub.edu>
2024-12-16 11:05:10 +0100

    Unit test comments: test borrar comentario si superuser

--- backend/app/tests/api/routes/test_comments.py
+++ backend/app/tests/api/routes/test_comments.py
@@ -26,7 +26,7 @@ def test_create_comment() -> None:
     """
     comment_data = {
         "text": "Comentario de prueba",
-        "user": 'user',
+        "user": 'test_user',
         "song_id": 1,
         "timestamp": datetime.utcnow().isoformat()
     }
@@ -46,3 +46,26 @@ def test_read_comment_by_id() -> None:
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == comment_id
+
+def test_delete_comment_super_user(client: TestClient, db: Session) -> None:
+    """
+    Test to delete a comment as a superuser.
+    """
+    # Crear un superusuario de prueba
+    username = "test1@superuser.com"
+    password = "Superuser1234"
+    user_in = UserTest(email=username, password=password, first_name="Test", second_name="Superuser", is_superuser=True, is_artist=False)
+    user = crud.user.create_user(session=db, user_create=user_in)
+
+    login_data = {"username": username, "password": password}
+    login_response = client.post(f"{settings.API_V1_STR}/login/access-token", data=login_data)
+    assert login_response.status_code == 200
+    access_token = login_response.json()["access_token"]
+    user_token_headers = {"Authorization": f"Bearer {access_token}"}
+
+    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+
+    response = client.delete(f"{settings.API_V1_STR}/comments/{comment.id}", headers=user_token_headers)
+    assert response.status_code == 200
+    deleted_comment = response.json()
+    assert deleted_comment["message"] == "Comment deleted successfully"
