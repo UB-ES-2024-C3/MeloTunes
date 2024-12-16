3788946f63f1b20a7b8b9b3f8d90a1a412a3ad3d
Sara <sromergo11@alumnes.ub.edu>
2024-12-16 10:40:45 +0100

    Unit test comments: test crear comentario

--- backend/app/tests/api/routes/test_comments.py
+++ backend/app/tests/api/routes/test_comments.py
@@ -5,6 +5,7 @@ from app.core.config import settings
 from app.models import CommentCreate, UserCreateOpen, Comment, UserTest
 from app import crud
 from app.tests.utils.utils import random_lower_string, random_email
+from datetime import datetime
 
 client = TestClient(app)
 comment_id = 0
@@ -18,3 +19,21 @@ def test_read_comments() -> None:
     data = response.json()
     assert "data" in data
     assert "count" in data
+
+def test_create_comment() -> None:
+    """
+    Test to create a new comment.
+    """
+    comment_data = {
+        "text": "Comentario de prueba",
+        "user": 'user',
+        "song_id": 1,
+        "timestamp": datetime.utcnow().isoformat()
+    }
+    response = client.post(f"{settings.API_V1_STR}/comments/", json=comment_data)
+    assert response.status_code == 200
+    data = response.json()
+    global comment_id
+    comment_id = data["id"]
+    assert data["text"] == comment_data["text"]
+    assert data["user"]== comment_data["user"]
